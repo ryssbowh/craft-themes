3aebcbb785ae47a5c490559c2b48c4819c7093fe
Boris Blondin <blondin.boris@gmx.com>
2021-09-30 07:21:48 +0100

    theme preferences comments

--- src/interfaces/ThemePreferencesInterface.php
+++ src/interfaces/ThemePreferencesInterface.php
@@ -2,6 +2,8 @@
 
 namespace Ryssbowh\CraftThemes\interfaces;
 
+use craft\elements\Asset;
+
 interface ThemePreferencesInterface 
 {
     /**
@@ -21,4 +23,152 @@ interface ThemePreferencesInterface
      * @return array
      */
     public function getLayoutAttributes(LayoutInterface $layout, bool $root = false): array;
+
+    /**
+     * Get block classes
+     * 
+     * @param  BlockInterface $block
+     * @return array
+     */
+    public function getBlockClasses(BlockInterface $block): array;
+
+    /**
+     * Get block attributes
+     * 
+     * @param  BlockInterface $block
+     * @return array
+     */
+    public function getBlockAttributes(BlockInterface $block): array;
+
+    /**
+     * Get region classes
+     * 
+     * @param  RegionInterface $region
+     * @return array
+     */
+    public function getRegionClasses(RegionInterface $region): array;
+
+    /**
+     * Get region attributes
+     * 
+     * @param  RegionInterface $region
+     * @return array
+     */
+    public function getRegionAttributes(RegionInterface $region): array;
+
+    /**
+     * Get field classes
+     * 
+     * @param  FieldInterface $field
+     * @return array
+     */
+    public function getFieldClasses(FieldInterface $field): array;
+
+    /**
+     * Get field attributes
+     * 
+     * @param  FieldInterface $field
+     * @return array
+     */
+    public function getFieldAttributes(FieldInterface $field): array;
+
+    /**
+     * Get field container classes
+     * 
+     * @param  FieldInterface $field
+     * @return array
+     */
+    public function getFieldContainerClasses(FieldInterface $field): array;
+
+    /**
+     * Get field container attributes
+     * 
+     * @param  FieldInterface $field
+     * @return array
+     */
+    public function getFieldContainerAttributes(FieldInterface $field): array;
+
+    /**
+     * Get field label classes
+     * 
+     * @param  FieldInterface $field
+     * @return array
+     */
+    public function getFieldLabelClasses(FieldInterface $field): array;
+
+    /**
+     * Get field label attributes
+     * 
+     * @param  FieldInterface $field
+     * @return array
+     */
+    public function getFieldLabelAttributes(FieldInterface $field): array;
+
+    /**
+     * Get group classes
+     * 
+     * @param  GroupInterface $group
+     * @return array
+     */
+    public function getGroupClasses(GroupInterface $group): array;
+
+    /**
+     * Get group attributes
+     * 
+     * @param  GroupInterface $group
+     * @return array
+     */
+    public function getGroupAttributes(GroupInterface $group): array;
+
+    /**
+     * Get group container classes
+     * 
+     * @param  GroupInterface $group
+     * @return array
+     */
+    public function getGroupContainerClasses(GroupInterface $group): array;
+
+    /**
+     * Get group container attributes
+     * 
+     * @param  GroupInterface $group
+     * @return array
+     */
+    public function getGroupContainerAttributes(GroupInterface $group): array;
+
+    /**
+     * Get group label classes
+     * 
+     * @param  GroupInterface $group
+     * @return array
+     */
+    public function getGroupLabelClasses(GroupInterface $group): array;
+
+    /**
+     * Get group container classes
+     * 
+     * @param  GroupInterface $group
+     * @return array
+     */
+    public function getGroupLabelAttributes(GroupInterface $group): array;
+
+    /**
+     * Get file classes
+     * 
+     * @param  Asset                  $asset
+     * @param  FieldInterface         $field
+     * @param  FileDisplayerInterface $displayer
+     * @return array
+     */
+    public function getFileClasses(Asset $asset, FieldInterface $field, FileDisplayerInterface $displayer): array;
+
+    /**
+     * Get file attributes
+     * 
+     * @param  Asset                  $asset
+     * @param  FieldInterface         $field
+     * @param  FileDisplayerInterface $displayer
+     * @return array
+     */
+    public function getFileAttributes(Asset $asset, FieldInterface $field, FileDisplayerInterface $displayer): array
 }
--- src/models/ThemePreferences.php
+++ src/models/ThemePreferences.php
@@ -11,18 +11,28 @@ use Ryssbowh\CraftThemes\interfaces\LayoutInterface;
 use Ryssbowh\CraftThemes\interfaces\RegionInterface;
 use Ryssbowh\CraftThemes\interfaces\ThemePreferencesInterface;
 use Ryssbowh\CraftThemes\interfaces\ViewModeInterface;
+use Ryssbowh\CraftThemes\services\ViewService;
 use craft\base\Component;
 use craft\elements\Asset;
 
 class ThemePreferences extends Component implements ThemePreferencesInterface
 {
+    /**
+     * @var ViewService
+     */
     protected $view;
 
+    /**
+     * @inheritDoc
+     */
     public function init()
     {
         $this->view = Themes::$plugin->view;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getLayoutClasses(LayoutInterface $layout, bool $root = false): array
     {
         $classes = ['layout', 'layout-' . $layout->type, 'view-mode-' . $this->viewMode->handle];
@@ -32,46 +42,73 @@ class ThemePreferences extends Component implements ThemePreferencesInterface
         return $classes;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getLayoutAttributes(LayoutInterface $layout, bool $root = false): array
     {
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getBlockClasses(BlockInterface $block): array
     {
         return ['block', 'block-' . $block->getMachineName()];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getBlockAttributes(BlockInterface $block): array
     {
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getRegionClasses(RegionInterface $region): array
     {
         return ['region', 'region-' . $region->handle];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getRegionAttributes(RegionInterface $region): array
     {
         return ['id' => 'region-' . $region->handle];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFieldClasses(FieldInterface $field): array
     {
         return ['field-content'];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFieldAttributes(FieldInterface $field): array
     {
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFieldContainerClasses(FieldInterface $field): array
     {
         return ['display', 'field', 'field-' . $field->handle, $field->displayer->handle];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFieldContainerAttributes(FieldInterface $field): array
     {
         if ($field->visuallyHidden) {
@@ -80,11 +117,17 @@ class ThemePreferences extends Component implements ThemePreferencesInterface
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFieldLabelClasses(FieldInterface $field): array
     {
         return ['field-label'];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFieldLabelAttributes(FieldInterface $field): array
     {
         if ($field->labelVisuallyHidden) {
@@ -93,11 +136,17 @@ class ThemePreferences extends Component implements ThemePreferencesInterface
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getGroupClasses(GroupInterface $group): array
     {
         return ['group-content'];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getGroupAttributes(GroupInterface $group): array
     {
         if ($group->visuallyHidden) {
@@ -106,11 +155,17 @@ class ThemePreferences extends Component implements ThemePreferencesInterface
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getGroupContainerClasses(GroupInterface $group): array
     {
         return ['display', 'group', 'group-' . $group->handle];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getGroupContainerAttributes(GroupInterface $group): array
     {
         if ($group->visuallyHidden) {
@@ -119,11 +174,17 @@ class ThemePreferences extends Component implements ThemePreferencesInterface
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getGroupLabelClasses(GroupInterface $group): array
     {
         return ['group-label'];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getGroupLabelAttributes(GroupInterface $group): array
     {
         if ($group->labelVisuallyHidden) {
@@ -132,31 +193,57 @@ class ThemePreferences extends Component implements ThemePreferencesInterface
         return [];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFileClasses(Asset $asset, FieldInterface $field, FileDisplayerInterface $displayer): array
     {
         return ['file', 'file-' . $displayer->handle];
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFileAttributes(Asset $asset, FieldInterface $field, FileDisplayerInterface $displayer): array
     {
         return [];
     }
 
+    /**
+     * View mode getter
+     * 
+     * @return ViewModeInterface
+     */
     protected function getViewMode(): ViewModeInterface
     {
         return $this->view->renderingViewMode;
     }
 
+    /**
+     * Layout getter
+     * 
+     * @return LayoutInterface
+     */
     protected function getLayout(): LayoutInterface
     {
         return $this->view->renderingLayout;
     }
 
+    /**
+     * Region getter
+     * 
+     * @return RegionInterface
+     */
     protected function getRegion(): RegionInterface
     {
         return $this->view->renderingRegion;
     }
 
+    /**
+     * Element getter
+     * 
+     * @return ?Element
+     */
     protected function getElement(): ?Element
     {
         return $this->view->renderingElement;
